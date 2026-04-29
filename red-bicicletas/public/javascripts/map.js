7ec909ec34e4b731ae8df2c34c7a272f0ecaec90
arerik2003-dev <arerik2003@gmail.com>
2026-04-28 22:49:44 -0600

    Pre

---
+++ red-bicicletas/public/javascripts/map.js
@@ -0,0 +1,5 @@
+var map = L.map('main_map').setView([19.4326, -99.1332], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
