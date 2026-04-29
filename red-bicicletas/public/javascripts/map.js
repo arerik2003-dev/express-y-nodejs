ffe56d29dab9c9bbef657c22bb8617a1759d3b13
arerik2003-dev <arerik2003@gmail.com>
2026-04-28 23:17:07 -0600

    Pagina principal

--- red-bicicletas/public/javascripts/map.js
+++ red-bicicletas/public/javascripts/map.js
@@ -1,5 +1,15 @@
-var map = L.map('main_map').setView([19.4326, -99.1332], 13);
+document.addEventListener('DOMContentLoaded', function() {
+    // 1. Inicializar el mapa
+    var map = L.map('main_map').setView([19.4335, -99.1915], 14);
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+    // 2. Cargar la capa de diseño
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; OpenStreetMap contributors'
+    }).addTo(map);
+
+    // 3. Agregar los puntos
+    L.marker([19.3243, -99.1742]).addTo(map).bindPopup('<b>Estación 1: Metro Universidad</b>');
+    L.marker([19.3311, -99.1821]).addTo(map).bindPopup('<b>Estación 4: Filosofía y Letras</b>');
+    L.marker([19.3345, -99.1825]).addTo(map).bindPopup('<b>Estación 6: Derecho</b>');
+    L.marker([19.3308, -99.1918]).addTo(map).bindPopup('<b>Estación 10: Estadio Olímpico</b>');
+});
